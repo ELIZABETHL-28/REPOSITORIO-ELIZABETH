f7d7de8d1c4b0b9ab2967e355e30e6c7f56dbe5a
Elizabeth <fernandaelibeth28@gmail.com>
2026-04-19 12:04:59 -0600

    creacion de funciones (def)  al pokemos  de fuego

--- PROYECTO POKEMON/pokemon_fuego.py
+++ PROYECTO POKEMON/pokemon_fuego.py
@@ -1,5 +1,21 @@
 from pokemon import Pokemon
 
 class PokemonFuego(Pokemon):
-     def __init__(self, nombre, hp_maximo, energia_maxima):
+    def __init__(self, nombre, hp_maximo, energia_maxima):
         super().__init__(nombre, hp_maximo, energia_maxima)
+
+    def atacar(self, enemigo): 
+        if self.energia_actual < 15:
+            print("LA ENERGIA NO ES SUFICIENTE")
+            return
+        
+        self.energia_actual -= 15
+        golpe = 15 
+
+        from pokemon_planta import PokemonPlanta
+        if isinstance(enemigo, PokemonPlanta): 
+            golpe *=2 
+            print("FUNCIONA DE MARAVILLA")
+            
+        enemigo.recibir_golpe(golpe)
+        
