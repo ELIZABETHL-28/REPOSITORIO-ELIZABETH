3cb0cb0f69afa43e8d239bfa253af3a9b05e895c
Elizabeth <fernandaelibeth28@gmail.com>
2026-04-19 18:06:56 -0600

    se llamo al catalogo pokedex.py

--- PROYECTO POKEMON/main.py
+++ PROYECTO POKEMON/main.py
@@ -1,11 +1,41 @@
+from pokedex import CATALOGO_POKEMON, mostrar_catalogo_disponible
 from pokemon_agua import PokemonAgua
 from pokemon_fuego import PokemonFuego
+from pokemon_planta import PokemonPlanta
+from pokemon_electrico import PokemonElectrico
+import random 
 
-criaturauno = PokemonAgua("Squirtle", 100, 50)
-criaturdos = PokemonFuego("Charmander", 100, 50)
+def crear_pokemon(opcion):
+    datos = CATALOGO_POKEMON[opcion]
 
-criaturauno.atacar(criaturdos)
-print(criaturdos.hp_actual)
+    if datos["tipo"] == "Fuego":
+        return PokemonFuego(datos["nombre"], datos["hp_maximo"], datos["energia_maxima"])
+
+    elif datos["tipo"] == "Agua":
+        return PokemonAgua(datos["nombre"], datos["hp_maximo"], datos["energia_maxima"])
+
+    elif datos["tipo"] == "Planta":
+        return PokemonPlanta(datos["nombre"], datos["hp_maximo"], datos["energia_maxima"])
+
+    elif datos["tipo"] == "Electrico":
+        return PokemonElectrico(datos["nombre"], datos["hp_maximo"], datos["energia_maxima"])
+    
+def elegir_accion():
+    print("1. Atacar")
+    print("2. Defender")
+    print("3. Descansar")
+
+    return input("Elige: ")
+
+def batalla(criaturauno, criaturados, modo): 
+    turno = 1 
+
+    while cri
+
+mostrar_catalogo_disponible()
+opcion = input("Elige tu Pokémon: ")
+jugador = crear_pokemon(opcion)
+print(f"Elegiste a {jugador.nombre}")
 
 # EN LA CLASE DE POKEMON TENEMOS QUE VER Y HACER UNA CLASE ABSTRACTA, Y TENGO QUE VER EN CUAL 
 # ME QUEDA MEJOR.
