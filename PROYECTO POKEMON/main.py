e89f118fe7656aa5e6b9cad5d660950eef819898
Elizabeth <fernandaelibeth28@gmail.com>
2026-04-19 23:41:18 -0600

    PROYECTO - funcion  de opcion a elegir PvP/PvE

--- PROYECTO POKEMON/main.py
+++ PROYECTO POKEMON/main.py
@@ -17,6 +17,26 @@ def crear_pokemon(opcion):
     elif datos["tipo"] == "Electrico":
         return PokemonElectrico(datos["nombre"], datos["hp_maximo"], datos["energia_maxima"])
 
+def seleccionar_modo(): 
+    while True: 
+        print("«««««««««««««««««««««««««««««««««««««««««««««")
+        print("SIMULADOR DE BATALLAS POKÉMON")
+        print("«««««««««««««««««««««««««««««««««««««««««««««")
+        print("1 - JUGADDOR VS JUGADOR")
+        print("2 - JUGADOR VS COMPUTADORA")
+
+        try: 
+            opcion = int(input("SELECCIONE EL MODO A JUGAR: "))
+            if opcion == 1 or opcion == 2: 
+                return opcion
+            else: 
+                print("OPCION FUERA DEL RANGO")
+        except ValueError:
+            print("ENTRADA INVALIDA. DEBE SER UN NUMERO")
+
+
+
+
 # Menú inicial
 mostrar_catalogo_disponible()
 opcion = input("ELIJE TU POKÉMON: ")
